d83a43cdd273492cf2b8e7c4d70f4c3e0f007555
JdM <julien@rmbi.ch>
2025-11-15 17:19:10 +0100

    fix

--- api/login_user.php
+++ api/login_user.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 include_once(dirname(__FILE__) . "/src/database.php");
 include_once(dirname(__FILE__) . "/src/models/user.php");
 include_once(dirname(__FILE__) . "/src/crypto.php");
@@ -12,7 +15,7 @@ $db = new Database();
 try {
     $user = $db->get_user(null, $username);
 } catch (Exception $e) {
-   // echo $e->getMessage();
+    // echo $e->getMessage();
     http_response_code(401);
     echo json_encode(["message" => "Invalid username."]);
     exit();
@@ -20,7 +23,7 @@ try {
 
 if (Crypto::verifyPassword($password, $user->getPasswordHash())) {
     Session::login($user);
-    
+
     http_response_code(200);
     echo json_encode(["message" => "Login successful."]);
     exit();
