67bb76948781877c32c5affa3df9b16c8618a6dd
JdM <julien@rmbi.ch>
2025-11-23 16:06:03 +0100

    updated sql strucure

--- structure.sql
+++ structure.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.3
+-- version 5.2.2
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Nov 19, 2025 at 07:16 PM
--- Server version: 9.3.0
--- PHP Version: 8.4.14
+-- Host: localhost:3306
+-- Generation Time: Nov 23, 2025 at 03:05 PM
+-- Server version: 11.4.8-MariaDB-deb12
+-- PHP Version: 8.3.24
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `cpneplace`
+-- Database: `julweb_cpneplace`
 --
 
 -- --------------------------------------------------------
@@ -28,11 +28,11 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `banned_users` (
-  `ID` int NOT NULL,
-  `user_ID` int NOT NULL,
+  `ID` int(11) NOT NULL,
+  `user_ID` int(11) NOT NULL,
   `end_date` datetime NOT NULL,
-  `reason` varchar(1000) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `moderator_ID` int NOT NULL
+  `reason` varchar(1000) NOT NULL,
+  `moderator_ID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -42,9 +42,9 @@ CREATE TABLE `banned_users` (
 --
 
 CREATE TABLE `images` (
-  `ID` int NOT NULL,
-  `post_ID` int NOT NULL,
-  `filename` varchar(1000) COLLATE utf8mb4_unicode_ci NOT NULL
+  `ID` int(11) NOT NULL,
+  `post_ID` int(11) NOT NULL,
+  `filename` varchar(1000) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -54,11 +54,11 @@ CREATE TABLE `images` (
 --
 
 CREATE TABLE `posts` (
-  `ID` int NOT NULL,
-  `content` varchar(10000) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `user_ID` int NOT NULL,
+  `ID` int(11) NOT NULL,
+  `content` varchar(10000) NOT NULL,
+  `user_ID` int(11) NOT NULL,
   `created_at` datetime NOT NULL,
-  `father_post_ID` int DEFAULT NULL
+  `father_post_ID` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -68,10 +68,10 @@ CREATE TABLE `posts` (
 --
 
 CREATE TABLE `reactions` (
-  `ID` int NOT NULL,
-  `user_ID` int NOT NULL,
-  `post_ID` int NOT NULL,
-  `reaction_type` int NOT NULL
+  `ID` int(11) NOT NULL,
+  `user_ID` int(11) NOT NULL,
+  `post_ID` int(11) NOT NULL,
+  `reaction_type` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -81,13 +81,25 @@ CREATE TABLE `reactions` (
 --
 
 CREATE TABLE `users` (
-  `ID` int NOT NULL,
-  `username` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `password_hash` varchar(1000) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `ID` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password_hash` varchar(1000) NOT NULL,
   `is_certified` tinyint(1) NOT NULL,
   `is_moderator` tinyint(1) NOT NULL,
-  `image_filename` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL
+  `image_filename` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `videos`
+--
+
+CREATE TABLE `videos` (
+  `ID` int(11) NOT NULL,
+  `post_ID` int(11) NOT NULL,
+  `filename` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 
 --
 -- Indexes for dumped tables
@@ -124,6 +136,12 @@ ALTER TABLE `users`
   ADD PRIMARY KEY (`ID`);
 
 --
+-- Indexes for table `videos`
+--
+ALTER TABLE `videos`
+  ADD PRIMARY KEY (`ID`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -131,31 +149,37 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `banned_users`
 --
 ALTER TABLE `banned_users`
-  MODIFY `ID` int NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `images`
 --
 ALTER TABLE `images`
-  MODIFY `ID` int NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `ID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `reactions`
 --
 ALTER TABLE `reactions`
-  MODIFY `ID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `ID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `videos`
+--
+ALTER TABLE `videos`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
